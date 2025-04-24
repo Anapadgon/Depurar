904f866fe467a6b6f852446889cf9591ed73f618
padilla.goana <anapadgonzalez@gmail.com>
2025-04-24 20:08:02 +0200

    Error al calcular la nota media

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,6 +10,9 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
+        if (estudiante.getNotas().length == 0) {
+            return Double.NaN;
+        }
         return suma / estudiante.getNotas().length; // Error si el array está vacío
     }
 
